1df9a2f759d268c2515a78d60e36e9349e56f112
FangQi-Jack <jackfangqi1314@gmail.com>
2017-02-28 14:40:36 +0800

    commit complete code

--- app/src/main/java/com/jackfangqi/canvasboard/MainActivity.java
+++ app/src/main/java/com/jackfangqi/canvasboard/MainActivity.java
@@ -15,8 +15,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
@@ -31,7 +29,6 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
     private ImageView canvasBoard;
 
     private Paint mPaint;
@@ -110,28 +107,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (mBitmap != null && !mBitmap.isRecycled()) {
             mBitmap.recycle();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
